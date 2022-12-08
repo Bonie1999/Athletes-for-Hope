cd30867969aaec984582944601d91cc59a167f2b
Puranjai Mendiratta <mendiratta.p@northeastern.edu>
2022-12-08 16:46:52 -0500

    Created Child.java

--- Athletes for Hope/src/business/WorkQueue/Child.java
+++ Athletes for Hope/src/business/WorkQueue/Child.java
@@ -4,10 +4,42 @@
  */
 package business.WorkQueue;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author puranjaimendiratta
  */
 public class Child {
-    
+     String childName;
+    Integer childAge;
+      ArrayList <Child> children= new ArrayList<Child>();
+
+    public ArrayList<Child> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<Child> children) {
+        this.children = children;
+    }
+    public void addChild(Child c)
+    {
+        children.add(c);
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public Integer getChildAge() {
+        return childAge;
+    }
+
+    public void setChildAge(Integer childAge) {
+        this.childAge = childAge;
+    }
 }
