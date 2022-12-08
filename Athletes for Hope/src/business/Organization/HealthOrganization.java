1972d7c3950efe32c0d632c17aeb4f1da0ddd5c0
Bonie Sachdev <sachdev.bo@northeastern.edu>
2022-12-08 18:25:24 -0500

    few more organizations completed

--- Athletes for Hope/src/business/Organization/HealthOrganization.java
+++ Athletes for Hope/src/business/Organization/HealthOrganization.java
@@ -4,10 +4,24 @@
  */
 package business.Organization;
 
+import business.Role.HealthInsuranceAgentRole;
+import business.Role.Role;
+import java.util.ArrayList;
 /**
  *
  * @author hp
  */
-public class HealthOrganization {
+public class HealthOrganization extends Organization{
+
+    public HealthOrganization() {
+        super(Organization.Type.Health.getValue());
+    }
+    
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new HealthInsuranceAgentRole());
+        return roles;
+    }
     
 }
