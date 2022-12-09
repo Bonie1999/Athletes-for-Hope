e2f1edc7c794885bfc099bbaaec77f4eca087310
NishankDave <dave.nish@northeastern.edu>
2022-12-08 22:17:28 -0500

    Updated NGOAdmin and NutrabayAdmin

--- Athletes for Hope/src/business/Role/NGOAdminRole.java
+++ Athletes for Hope/src/business/Role/NGOAdminRole.java
@@ -3,11 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business.Role;
-
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Network.Network;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import ui.EnterpriseAdmin.EnterpriseAdminJPanel;
 /**
  *
  * @author nishank
  */
 public class NGOAdminRole {
-    
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+        return new EnterpriseAdminJPanel(userProcessContainer, enterprise, business);
+    }
 }
