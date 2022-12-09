28c977c76b038921e7b9cd84225d2b923754816a
NishankDave <dave.nish@northeastern.edu>
2022-12-08 22:14:18 -0500

    Update MentalHealthCoachRole.java

--- Athletes for Hope/src/business/Role/MentalHealthCoachRole.java
+++ Athletes for Hope/src/business/Role/MentalHealthCoachRole.java
@@ -3,11 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business.Role;
-
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Network.Network;
+import business.Organization.PsychiatricOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import ui.Psychiatrist.PsychiatristJPanel;
+import javax.swing.JPanel;
 /**
  *
  * @author nishank
  */
 public class MentalHealthCoachRole {
-    
+      @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+        return new PsychiatristJPanel(userProcessContainer, account, organization,enterprise,business,network);
+    }
 }
