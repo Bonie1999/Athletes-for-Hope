e876a0de271e584731d5a8eacbc4569d751c227b
Puranjai Mendiratta <mendiratta.p@northeastern.edu>
2022-12-08 21:01:21 -0500

    Updated and completed InsuranceAgentWorkRequest.java

--- Athletes for Hope/src/business/WorkQueue/InsuranceAgentWorkRequest.java
+++ Athletes for Hope/src/business/WorkQueue/InsuranceAgentWorkRequest.java
@@ -4,10 +4,32 @@
  */
 package business.WorkQueue;
 
+import business.Consultation.ConsultationHealthInsuranceAgent;
+import java.util.ArrayList;
+
+
 /**
  *
- * @author puranjaimendiratta
+ * @author Puranjai
  */
-public class InsuranceAgentWorkRequest {
+public class InsuranceAgentWorkRequest extends WorkRequest {
     
+    TalentScoutWorkRequest TalentScoutWorkRequest;
+    ArrayList<ConsultationHealthInsuranceAgent> HealthInsuranceAgentConsultation = new ArrayList<ConsultationHealthInsuranceAgent>();
+
+    public TalentScoutWorkRequest getTalentScoutWorkRequest() {
+        return TalentScoutWorkRequest;
+    }
+
+    public void setTalentScoutWorkRequest(TalentScoutWorkRequest TalentScoutWorkRequest) {
+        this.TalentScoutWorkRequest = TalentScoutWorkRequest;
+    }
+
+    public ArrayList<ConsultationHealthInsuranceAgent> getHealthInsuranceAgentConsultation() {
+        return HealthInsuranceAgentConsultation;
+    }
+
+    public void setHealthInsuranceAgentConsultation(ArrayList<ConsultationHealthInsuranceAgent> HealthInsuranceAgentConsultation) {
+        this.HealthInsuranceAgentConsultation = HealthInsuranceAgentConsultation;
+    }
 }
