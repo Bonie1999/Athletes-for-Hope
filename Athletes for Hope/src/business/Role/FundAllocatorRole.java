5efd8f55ef03551134ed87c98265fdf98edeebba
NishankDave <dave.nish@northeastern.edu>
2022-12-08 22:09:22 -0500

    Updated FundAllocatorRole

--- Athletes for Hope/src/business/Role/FundAllocatorRole.java
+++ Athletes for Hope/src/business/Role/FundAllocatorRole.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business.Role;
-
+import javax.swing.JPanel;
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Network.Network;
+import business.Organization.CounsellingOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import business.WorkQueue.CaseReporterWorkRequest; 
+import javax.swing.JPanel;
+import ui.Counsellor.CounsellorJPanel;
 /**
  *
  * @author nishank
  */
 public class FundAllocatorRole {
-    
+     @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization,Enterprise enterprise, EcoSystem business,Network network) {
+        return new CounsellorJPanel(userProcessContainer, account, organization,enterprise,business,network);
+    }
 }
