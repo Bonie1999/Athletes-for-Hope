48760eaa3ca1c9dfc8f952c4bb02cc8a142a2d08
NishankDave <dave.nish@northeastern.edu>
2022-12-08 21:43:38 -0500

    Updated ConsultationMentalHealthCoach

--- Athletes for Hope/src/business/Consultation/ConsultationMentalHealthCoach.java
+++ Athletes for Hope/src/business/Consultation/ConsultationMentalHealthCoach.java
@@ -9,5 +9,35 @@ package business.Consultation;
  * @author nishank
  */
 public class ConsultationMentalHealthCoach {
+    private String consultation;
+    private String guidance;
+    private String progress;
+
+    public ConsultationMentalHealthCoach(){
     
+    }
+
+    public String getConsultation() {
+        return consultation;
+    }
+
+    public void setConsultation(String consultation) {
+        this.consultation = consultation;
+    }
+
+    public String getGuidance() {
+        return guidance;
+    }
+
+    public void setGuidance(String guidance) {
+        this.guidance = guidance;
+    }
+
+    public String getProgress() {
+        return progress;
+    }
+
+    public void setProgress(String progress) {
+        this.progress = progress;
+    }
 }
