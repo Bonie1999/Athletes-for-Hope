1f7a4583fe2ca964edf5f641b335c5adc0447354
Puranjai Mendiratta <mendiratta.p@northeastern.edu>
2022-12-08 21:36:12 -0500

    Updated and completed LabTest.java

--- Athletes for Hope/src/business/WorkQueue/LabTest.java
+++ Athletes for Hope/src/business/WorkQueue/LabTest.java
@@ -6,8 +6,35 @@ package business.WorkQueue;
 
 /**
  *
- * @author puranjaimendiratta
+ * @author Puranjai
  */
+
 public class LabTest {
+    private String test;
+    private String Result;
+
+    public LabTest(String test) {
+        this.test = test;
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+
+    public String getResult() {
+        return Result;
+    }
+
+    public void setResult(String Result) {
+        this.Result = Result;
+    }
     
+    @Override
+    public String toString() {
+        return this.getTest();
+    }
 }
