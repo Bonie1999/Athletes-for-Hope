e25b1cff54a5b7ec0b681457bf66ca5c2bdf93c6
NishankDave <dave.nish@northeastern.edu>
2022-12-08 22:30:40 -0500

    Update SupplementSalesRepRole.java

--- Athletes for Hope/src/business/Role/SupplementSalesRepRole.java
+++ Athletes for Hope/src/business/Role/SupplementSalesRepRole.java
@@ -3,11 +3,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business.Role;
+import business.EcoSystem;
+import business.Enterprise.Enterprise;
+import business.Network.Network;
+import business.Organization.PharmacyOrganization;
+import business.Organization.Organization;
+import business.UserAccount.UserAccount;
+import ui.Pharmacy.PharmacistJPanel;
+import javax.swing.JPanel;
 
 /**
  *
  * @author nishank
  */
 public class SupplementSalesRepRole {
-    
+        @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business,Network network) {
+        return new PharmacistJPanel(userProcessContainer, account, organization,enterprise,business);
+    }
+
 }
